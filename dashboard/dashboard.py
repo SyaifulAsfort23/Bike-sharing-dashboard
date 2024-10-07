ce38bcf913370e437ecce19a1831205d3c25a676
Syaiful_Kusing <146799911+SyaifulAsfort23@users.noreply.github.com>
2024-10-07 08:33:29 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import scikit-learn 
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
